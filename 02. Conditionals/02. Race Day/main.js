689ba99cb738973a0a6f95bb1f85ee0f6b225104
Mango <82413197+enlistedmango@users.noreply.github.com>
2022-11-03 23:15:51 +0000

    updating readme

--- 02. Conditionals/02. Race Day/main.js
+++ 02. Conditionals/02. Race Day/main.js
@@ -7,7 +7,6 @@ if (earlyRegistration && age > 18) {
     raceNumber += 1000;
 }
 
-
 if (earlyRegistration && age > 18) {
     console.log(`The race will start at 9:30am and your race number is: ${raceNumber}.`);
 } else if (!earlyRegistration && age > 18) {
